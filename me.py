6a1980b0bce477bc0c6425ba9a248406dc52bd39
Lil Marvel <lilmarvelmedia@gmail.com>
2026-09-22 12:25:40 +0100

    add Python project

---
+++ me.py
@@ -0,0 +1,9 @@
+print("my first python program")
+name = "marvel"
+age = 16
+print(name)
+print(age)
+
+name = input("What is your name")
+age = input("What is your age")
+page = input("What is your age")
